4850c694eca870acf5ed0416e0a4900d9ecd873c
sonpa2002 <119403398+sonpa2002@users.noreply.github.com>
2024-06-13 16:36:33 +0700

    [complete][slider]

---
+++ js/slider.js
@@ -0,0 +1,93 @@
+
+const sliderItems = [
+    document.querySelector('.sliderMain .Item1'),
+    document.querySelector('.sliderMain .Item2'),
+    document.querySelector('.sliderMain .Item3'),
+    document.querySelector('.sliderMain .Item4')
+];
+var arraySlider = ["sliderMain__smallItem Item1", "sliderMain__smallItem Item2", "sliderMain__smallItem Item3", "sliderMain__smallItem Item4"];
+var animateTime = 0.40; var IntervalTime = 1.00; var IntervalSliderID = 0;
+// console.log(Math.floor(IntervalTime*1000));
+function CustomSlider(SetAnimateTime, SetIntervalTime){
+    animateTime = SetAnimateTime; IntervalTime = SetIntervalTime;
+
+    for (let i = 0; i < sliderItems.length; i++) {
+        sliderItems[i].style.animationDuration = (animateTime.toFixed(2) + "s");
+    }
+}
+function fireSlider(SetAnimateTime, SetIntervalTime, arrPhoto, animateStyle){
+    CustomSlider(SetAnimateTime, SetIntervalTime);
+    if(animateStyle === "ChangePhoto"){
+        if(arrPhoto.length > 1){
+            let nextPhoto = 0;
+            let sliderItemImg;
+            IntervalSliderID = setInterval(function (){
+
+                let takeSliderItem = sliderItems.shift();
+                sliderItems.push(takeSliderItem);
+        
+                for (let i = 0; i < sliderItems.length; i++) {
+                    sliderItems[i].setAttribute("class", arraySlider[i]);
+                }
+
+                for (let i = 0; i < sliderItems.length; i++) {
+                    sliderItemImg = sliderItems[i].querySelector('img');
+                    sliderItemImg.setAttribute('src', sliderItemImg.getAttribute("src").replace(/[A-G][1-4]\.jpg/g, (`${arrPhoto[nextPhoto]}${i==0 ? 4 : i}.jpg`)));
+                }
+                nextPhoto++;
+                if(nextPhoto >= arrPhoto.length){
+                    nextPhoto = 0;
+                }
+            }, Math.floor(IntervalTime*1000))
+        }
+        else if(arrPhoto.length === 1){
+            let sliderItemImg;
+            for (let i = 0; i < sliderItems.length; i++) {
+                sliderItemImg = sliderItems[i].querySelector('img');
+                sliderItemImg.setAttribute('src', sliderItemImg.getAttribute("src").replace(`B${i + 1}.jpg`, (`${arrPhoto[0]}${i + 1}.jpg`)));
+            }
+            IntervalSliderID = setInterval(function (){
+                let takeSliderItem = sliderItems.shift();
+                sliderItems.push(takeSliderItem);
+        
+                for (let i = 0; i < sliderItems.length; i++) {
+                    sliderItems[i].setAttribute("class", arraySlider[i]);
+                }
+            }, Math.floor(IntervalTime*1000))
+        }
+        else
+        {
+            IntervalSliderID = setInterval(function (){
+                let takeSliderItem = sliderItems.shift();
+                sliderItems.push(takeSliderItem);
+        
+                for (let i = 0; i < sliderItems.length; i++) {
+                    sliderItems[i].setAttribute("class", arraySlider[i]);
+                }
+            }, Math.floor(IntervalTime*1000))
+        }
+    }
+    else if(animateStyle === "Default"){
+        if(arrPhoto.length > 0){
+            let sliderItemImg;
+            for (let i = 0; i < sliderItems.length; i++) {
+                sliderItemImg = sliderItems[i].querySelector('img');
+                sliderItemImg.setAttribute('src', sliderItemImg.getAttribute("src").replace(`B${i + 1}.jpg`, (`${arrPhoto[0]}${i + 1}.jpg`)));
+            }
+        }
+        IntervalSliderID = setInterval(function (){
+            let takeSliderItem = sliderItems.shift();
+            sliderItems.push(takeSliderItem);
+    
+            for (let i = 0; i < sliderItems.length; i++) {
+                sliderItems[i].setAttribute("class", arraySlider[i]);
+            }
+        }, Math.floor(IntervalTime*1000))
+    }
+
+}
+function stopSlider(){
+    clearInterval(IntervalSliderID);
+}
+
+fireSlider(0.4, 1.00, ["A","B","C","D","E","F","G"], "ChangePhoto");
